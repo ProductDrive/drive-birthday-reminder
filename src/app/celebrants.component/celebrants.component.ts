d731bb1d397077838e9d8fdc82abbacb7373f7ed
afeexclusive <afeexclusive@gmail.com>
2026-02-22 23:28:18 +0000

    refactor celebrant page

--- src/app/celebrants.component/celebrants.component.ts
+++ src/app/celebrants.component/celebrants.component.ts
@@ -4,20 +4,27 @@ import { CelebrantsService, Celebrant } from '../services/celebrants.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Auth } from '@angular/fire/auth';
+import { CdkAriaLive } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-celebrants',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, AsyncPipe, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, AsyncPipe, MatIconModule, MatInputModule, MatFormFieldModule, CdkAriaLive],
   templateUrl: './celebrants.component.html',
   styleUrls: ['./celebrants.component.scss'],
 })
 export class CelebrantsComponent implements OnInit {
   celebrants$!: Observable<Celebrant[]>;
+  get inviteLink(): string {
+    const user = this.auth.currentUser;
+    return user ? `${window.location.origin}/form/${user.uid}` : '';
+  }
 
   constructor(
     private celebrantsService: CelebrantsService,
@@ -63,15 +70,20 @@ export class CelebrantsComponent implements OnInit {
       window.open(url, '_blank');
   }
 
+  addCelebrant() {
+    const user = this.auth.currentUser;
+    if (user) {
+      this.router.navigate(['/form', user.uid]);
+    }
+  }
+
   copyInviteLink() {
     const user = this.auth.currentUser;
     if (!user) {
       alert('You must be logged in to copy invite link.');
       return;
     }
-
-    const link = `${window.location.origin}/form/${user.uid}`;
-    navigator.clipboard.writeText(link);
+    navigator.clipboard.writeText(this.inviteLink);
     alert('Invite link copied to clipboard!');
   }
 
