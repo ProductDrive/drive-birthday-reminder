7e7fcfd6074b7cce5c9bb315c5d678cce03023c9
afeexclusive <afeexclusive@gmail.com>
2026-03-14 03:57:01 +0000

    repaired

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5002',
-  firebase: {"projectId":"afebdayrem","appId":"1:531316247978:web:96f441523b5bcacd622fd3","storageBucket":"afebdayrem.firebasestorage.app","apiKey":"AIzaSyAw8B4NLZ-M4tIS8UnODMOtXqE6qUc-4NM","authDomain":"afebdayrem.firebaseapp.com","messagingSenderId":"531316247978"}
+  apiUrl: process.env['NG_APP_API_URL'] || 'http://localhost:5002',
+  firebase: {
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || 'afebdayrem',
+    appId: process.env['NG_APP_FIREBASE_APP_ID'] || '1:531316247978:web:96f441523b5bcacd622fd3',
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || 'afebdayrem.firebasestorage.app',
+    apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || 'AIzaSyAw8B4NLZ-M4tIS8UnODMOtXqE6qUc-4NM',
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || 'afebdayrem.firebaseapp.com',
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '531316247978'
+  }
 };
