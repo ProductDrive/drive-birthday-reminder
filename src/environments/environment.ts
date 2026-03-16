f02331cd31425fa1d92c6b6805d2f39e1af1a121
afeexclusive <afeexclusive@gmail.com>
2026-03-16 06:34:14 +0000

    trial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,25 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: process.env['NG_APP_API_URL'],
+//   firebase: {
+//     projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+//     appId: process.env['NG_APP_FIREBASE_APP_ID'],
+//     storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+//     apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+//     authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+//     messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID']
+//   }
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: process.env['NG_APP_API_URL'],
+  production: true,
+  apiUrl: 'NG_APP_API_URL_PLACEHOLDER',
   firebase: {
-    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
-    appId: process.env['NG_APP_FIREBASE_APP_ID'],
-    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
-    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID']
+    projectId: 'NG_APP_FIREBASE_PROJECT_ID_PLACEHOLDER',
+    appId: 'NG_APP_FIREBASE_APP_ID_PLACEHOLDER',
+    storageBucket: 'NG_APP_FIREBASE_STORAGE_BUCKET_PLACEHOLDER',
+    apiKey: 'NG_APP_FIREBASE_API_KEY_PLACEHOLDER',
+    authDomain: 'NG_APP_FIREBASE_AUTH_DOMAIN_PLACEHOLDER',
+    messagingSenderId: 'NG_APP_FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER'
   }
 };
