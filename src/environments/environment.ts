8ee4a982e314d094e623fcbe2671469b7ab01844
afeexclusive <afeexclusive@gmail.com>
2026-03-17 23:03:34 +0000

    facelift for modal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,16 +10,3 @@
 //     messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID']
 //   }
 // };
-
-export const environment = {
-  production: true,
-  apiUrl: 'NG_APP_API_URL_PLACEHOLDER',
-  firebase: {
-    projectId: 'NG_APP_FIREBASE_PROJECT_ID_PLACEHOLDER',
-    appId: 'NG_APP_FIREBASE_APP_ID_PLACEHOLDER',
-    storageBucket: 'NG_APP_FIREBASE_STORAGE_BUCKET_PLACEHOLDER',
-    apiKey: 'NG_APP_FIREBASE_API_KEY_PLACEHOLDER',
-    authDomain: 'NG_APP_FIREBASE_AUTH_DOMAIN_PLACEHOLDER',
-    messagingSenderId: 'NG_APP_FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER'
-  }
-};
