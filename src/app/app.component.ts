f06fa6e37fb5afcee1a6c4ad1645aa0d4833d467
afeexclusive <afeexclusive@gmail.com>
2026-05-30 11:12:22 +0100

    fixed PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PwaInstallBannerComponent } from './pwa-install-banner/pwa-install-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, PwaInstallBannerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
