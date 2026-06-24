1f9cc0a1a8114daeb50ff520d985ae5a0e58c087
Adeoye Temitayo <adeoyetemitayo99@gmail.com>
2026-06-24 14:54:16 +0100

    add landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { CelebrantsComponent } from './celebrants.component/celebrants.component';
 import { BirthdayFormComponent } from './birthday-form/birthday-form.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
+    { path: '', component: LandingPageComponent, pathMatch: 'full' },
     { path: 'auth', component: AuthComponent },
     { path: 'celebrants', component: CelebrantsComponent, canActivate: [authGuard] },
     { path: 'form/:userId', component: BirthdayFormComponent },
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
 ];
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './landing-page.component.html',
+})
+export class LandingPageComponent {}
